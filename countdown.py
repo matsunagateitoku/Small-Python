d5b6580edfcd2f5f3a3aceb65e70fa86d4321d23
Scott <scottlinebergerbeloit@gmail.com>
2022-09-12 13:06:08 -0400

    first update r

---
+++ countdown.py
@@ -0,0 +1,42 @@
+import sys, time
+import sevseg
+
+secondsLeft = 30
+
+try:
+        while True:
+            print('\n' * 60)
+
+            hours = str(secondsLeft // 3600)
+            minutes=str((secondsLeft % 3600) // 60)
+            seconds= str(secondsLeft % 60)
+
+            hDigits = sevseg.getSevSegStr(hours, 2)
+            hTopRow, hMiddleRow, hBottomRow = hDigits.splitlines()
+
+        
+
+            mDigits = sevseg.getSevSegStr(minutes, 2)
+            mTopRow, mMiddleRow, mBottomRow = mDigits.splitlines()
+            sDigits = sevseg.getSevSegStr(seconds, 2)
+            sTopRow, sMiddleRow, sBottomRow, = sDigits.splitlines()
+
+            print(hTopRow  + "      " + mTopRow    + "      "  + sTopRow)
+            print(hMiddleRow  + "   *   " + mMiddleRow    + "    *  "  + sMiddleRow)
+            print(hBottomRow  + "   *   " + mBottomRow    + "    *  "  + sBottomRow)
+
+            if secondsLeft  == 0:
+                print()
+                print("      ********BOOM********")
+                break
+            print()
+            print('Press Ctrl-C to quit.')
+
+            time.sleep(1)
+            secondsLeft    -= 1
+except KeyboardInterrupt:
+            print("Countdown")
+            sys.exit()
+
+
+    
